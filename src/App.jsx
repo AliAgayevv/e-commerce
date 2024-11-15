6a81f92cb31606bf982dc2393c37cde18714f162
AliAgayevv <aghayev_233@mail.ru>
2024-11-16 02:25:08 +0400

    aboutus filter by object / 404 page footer removed DONE

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Landing from "./components/Landing";
 import LatestProducts from "./components/Latest Produc";
 import Contact from "./components/Contact";
 import Login from "./components/Login";
-import Register from "./components/Register"
+import Register from "./components/Register";
 import AboutUs from "./components/AboutUs";
 import Footer from "./components/Footer";
 import NotFound from "./components/NotFound";
@@ -14,18 +14,30 @@ function App() {
   return (
     <Router>
       <Navbar />
+      <MainRoutes />
+    </Router>
+  );
+}
+
+function MainRoutes() {
+  const location = useLocation(); 
+  const isNotFoundPage = !["/", "/home", "/about", "/products", "/contact", "/login", "/register"].includes(location.pathname);
+
+  return (
+    <>
       <Routes>
-        <Route path="/" element={[<Landing/>, <LatestProducts/>]} />
-        <Route path="/home" element={[<Landing/>, <LatestProducts/>]} />
-        <Route path="/about" element={<AboutUs/>} />
+        <Route path="/" element={[<Landing />, <LatestProducts />]} />
+        <Route path="/home" element={[<Landing />, <LatestProducts />]} />
+        <Route path="/about" element={<AboutUs />} />
         <Route path="/products" element={<LatestProducts />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="*" element={<NotFound/>} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-      <Footer/>
-    </Router>
+
+      {!isNotFoundPage && <Footer />}
+    </>
   );
 }
 
--- src/components/AboutUs/index.jsx
+++ src/components/AboutUs/index.jsx
@@ -2,34 +2,58 @@ import React from 'react'
 import CustomHR from '../CustomHR'
 import AboutUsCart from "../AboutUsCart"
 
+const aboutUsData = [
+    {
+        id: 1,
+        photoPath: "./src/assets/imgs/manclot.jpeg",
+        title: "Men's Clothing"
+    },
+    {
+        id: 2,
+        photoPath: "./src/assets/imgs/womencloth.jpeg",
+        title: "Women's Clothing"
+    },
+    {
+        id: 3,
+        photoPath: "./src/assets/imgs/jewelery.webp",
+        title: "Jewelery"
+    },
+    {
+        id: 4,
+        photoPath: "./src/assets/imgs/electronics.jpeg",
+        title: "Electronics"
+    }]
+
 export default function AboutUs() {
-  return (
-    <div className='flex flex-col mt-10'>
-       <h1 className='text-center text-4xl'>
-        About Us
-        </h1> 
-        <CustomHR />
+    return (
+        <div className='flex flex-col mt-10'>
+            <h1 className='text-center text-4xl'>
+                About Us
+            </h1>
+            <CustomHR />
 
-<div className='w-11/12 mx-auto mt-5'>
-<p className='text-xl font-light text-center'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum facere doloremque veritatis odit similique sequi. Odit amet fuga nam quam quasi facilis sed doloremque saepe sint perspiciatis explicabo totam vero quas provident ipsam, veritatis nostrum velit quos recusandae est mollitia esse fugit dolore laudantium. Ex vel explicabo earum unde eligendi autem praesentium, doloremque distinctio nesciunt porro tempore quis eaque labore voluptatibus ea necessitatibus exercitationem tempora molestias. Ad consequuntur veniam sequi ullam tempore vel tenetur soluta dolore sunt maxime aliquam corporis est, quo saepe dolorem optio minus sint nemo totam dolorum! Reprehenderit delectus expedita a alias nam recusandae illo debitis repellat libero, quasi explicabo molestiae saepe, dolorem tempore itaque eveniet quam dignissimos blanditiis excepturi harum numquam vel nihil? Ipsum
+            <div className='w-11/12 mx-auto mt-5'>
+                <p className='text-xl font-light text-center'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum facere doloremque veritatis odit similique sequi. Odit amet fuga nam quam quasi facilis sed doloremque saepe sint perspiciatis explicabo totam vero quas provident ipsam, veritatis nostrum velit quos recusandae est mollitia esse fugit dolore laudantium. Ex vel explicabo earum unde eligendi autem praesentium, doloremque distinctio nesciunt porro tempore quis eaque labore voluptatibus ea necessitatibus exercitationem tempora molestias. Ad consequuntur veniam sequi ullam tempore vel tenetur soluta dolore sunt maxime aliquam corporis est, quo saepe dolorem optio minus sint nemo totam dolorum! Reprehenderit delectus expedita a alias nam recusandae illo debitis repellat libero, quasi explicabo molestiae saepe, dolorem tempore itaque eveniet quam dignissimos blanditiis excepturi harum numquam vel nihil? Ipsum
 
 
-        </p>
+                </p>
 
-        <h2 className='text-center mt-20 text-3xl'>
-            Our Products
-        </h2>
+                <h2 className='text-center mt-20 text-3xl'>
+                    Our Products
+                </h2>
 
-        <div className='flex items-center justify-center gap-6 mt-8'>
-            <AboutUsCart name={"Men's Clothing"} imgPath={"./src/assets/imgs/manclot.jpeg"} />
-            <AboutUsCart name={"Women's Clothing"} imgPath={"./src/assets/imgs/womencloth.jpeg"} />
-            <AboutUsCart name={"Jewelery"} imgPath={"./src/assets/imgs/jewelery.webp"} />
-            <AboutUsCart name={"Electronics"} imgPath={"./src/assets/imgs/electronics.jpeg"} />
+                <div className='flex items-center justify-center gap-6 mt-8'>
+                    {
+                        aboutUsData.map((item) => {
+                            return <AboutUsCart name={item.title} imgPath={item.photoPath} />
+                        })
+
+                    }
+                </div>
+            </div>
+
+
         </div>
-        </div>
-      
-
-    </div>
-  )
+    )
 }
