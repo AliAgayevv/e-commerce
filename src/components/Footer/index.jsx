12c6fb9d90670ffef2eb0c22c32cd74a6f92f24a
AliAgayevv <aghayev_233@mail.ru>
2024-11-25 05:10:44 +0400

    [FIXED] - Footer link target change to _blank for better UX

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,9 +5,9 @@ import { FaGithub } from "react-icons/fa";
 export default function Footer() {
   return (
     <div className='text-center gap-2 flex text-2xl flex-col items-center justify-center mt-24'>
-        <p>Made by <a className='underline' href='https://github.com/AliAgayevv'>Ali</a>
+        <p>Made by <a className='underline' target="_blank" href='https://github.com/AliAgayevv'>Ali</a>
         </p>
-        <a href='https://github.com/AliAgayevv'>
+        <a href='https://github.com/AliAgayevv' target="_blank">
         <FaGithub size={"20px"}/>
         </a>
     </div>
